106587798fa21c7eaf63a91e786c32960f1b5bb6
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-05-10 11:53:18 +0300

    Add fillable property to stock movements

--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -8,6 +8,10 @@ class StockMovement extends Model
 {
     protected $fillable = [
         'stock_id',
+        'material_request_item_id',
+        'quotation_item_id',
+        'stockable_id',
+        'stockable_type',
         'in',
         'out',
     ];
