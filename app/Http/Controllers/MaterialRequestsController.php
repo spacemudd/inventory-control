08cce08b3393ef63027a8a7217b5a4a4d4ce46b5
Taron <taronambartsumian7@gmail.com>
2019-05-15 11:50:05 +0400

    conflict fix

--- app/Http/Controllers/MaterialRequestsController.php
+++ app/Http/Controllers/MaterialRequestsController.php
@@ -63,7 +63,6 @@ class MaterialRequestsController extends Controller
         }
 
         $materialRequest = $this->service->save($request->except('_token'));
-
         return redirect()->route('material-requests.show', ['id' => $materialRequest->id]);
     }
 
---
+++ resources/assets/js/components/MaterialRequestItemsContainer/SelectDateForMaterialRequest.vue
@@ -0,0 +1,31 @@
+<template>
+         <b-field>
+                <b-datepicker
+                        v-model="date"
+                        placeholder="Click to select...">
+                </b-datepicker>
+             <input type="hidden" name="date" id="date" :value="newVal">
+        </b-field>
+</template>
+<script>
+    import moment from 'moment';
+
+    export default {
+      data() {
+        return {
+          date:  new Date(),
+          newVal: ''
+        }
+      },
+      mounted() {
+        this.newVal = moment(this.date).format('DD-MM-YYYY');
+      },
+      watch: {
+        date: {
+         handler: function(val, oldVal) {
+           this.newVal = moment(val).format('DD-MM-YYYY');
+         }
+        }
+      },
+    }
+</script>
