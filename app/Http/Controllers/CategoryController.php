01190fe29e94f0b77e79444658749371bc4a3fbd
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-05-30 13:04:17 +0300

    Add categories page, add stock entry

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::get();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('categories.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+        ]);
+
+        $c = Category::create($request->except('_token'));
+
+        return redirect()->route('categories.index');
+    }
+}
---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.app', ['title' => 'New category'])
+
+
+@section('header')
+	<nav class="breadcrumb" aria-label="breadcrumbs">
+		<ul>
+			<li>
+				<a href="{{ route('dashboard.index') }}" aria-current="page">
+					<span class="icon is-small"><i class="fa fa-inbox"></i></span>
+					<span>{{ trans('words.dashboard') }}</span>
+				</a>
+			</li>
+			<li>
+				<a href="{{ route('categories.index') }}">
+					<span class="icon is-small"><i class="fa fa-bookmark"></i></span>
+					<span>Categories</span>
+				</a>
+			</li>
+			<li class="is-active">
+				<a href="#">New category</a>
+			</li>
+		</ul>
+	</nav>
+@endsection
+
+@section('content')
+
+	@if(session()->has('success'))
+		<div class="columns">
+			<div class="column is-4 is-offset-4">
+				<div class="notification is-primary">
+					{{ session()->get('success') }}
+				</div>
+			</div>
+		</div>
+	@endif
+
+	<form method="post" action="{{ route('categories.store') }}">
+		@csrf
+		<div class="columns is-multiline">
+			<div class="column is-4 is-offset-4">
+				<div class="field">
+					<label for="name" class="label">{{ trans('words.name') }} <span class="has-text-danger">*</span></label>
+
+					<p class="control">
+						<input id="description" type="text"
+							   class="input {{ $errors->has('name') ? ' is-danger' : '' }}"
+							   name="name"
+							   value="{{ old('name') }}"
+							   autofocus
+							   required>
+
+						@if ($errors->has('name'))
+							<span class="help is-danger">
+                                {{ $errors->first('name') }}
+                            </span>
+						@endif
+					</p>
+				</div>
+			</div>
+
+			<div class="column is-4 is-offset-4 has-text-right">
+				<a class="button is-text" href="{{ url('/') }}">Cancel</a>
+				<button type="submit" class="button is-success">{{ trans('words.save') }}</button>
+			</div>
+
+		</div>
+
+	</form>
+
+@endsection
