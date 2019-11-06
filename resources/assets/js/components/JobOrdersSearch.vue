2ab384505583e04539b539d4d85ee3cb471bf21d
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-11-06 14:21:59 +0300

    Show job orders items in the search box

--- resources/assets/js/components/JobOrdersSearch.vue
+++ resources/assets/js/components/JobOrdersSearch.vue
@@ -19,6 +19,27 @@
 
         <!--Results-->
         <div class="dropdown-menu" id="dropdown-menu" role="menu" v-if="open && results.length">
+            <div class="items-container box" v-if="showingItems.length">
+                <table class="table is-size-7 is-narrow is-fullwidth">
+                    <colgroup>
+                        <col>
+                        <col style="width:20px;">
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th>Description</th>
+                        <th class="has-text-right">Quantity</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr v-for="item in showingItems">
+                        <td>{{ item.stock.description }}</td>
+                        <td class="has-text-right">{{ item.qty }}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+
             <div class="dropdown-content">
                 <div v-for="result in results">
                     <a
@@ -33,6 +54,10 @@
                                 <div class="stats-value" style="display:block;" v-if="result.department">
                                     <p>Cost Center</p>
                                     <p>{{ result.department.code }}</p>
+                                </div>
+                                <div class="button is-primary is-small button-thin"
+                                        @mouseenter="showItems(result.items)" @mouseleave="removeItems">
+                                    View items ({{ result.items.length }})
                                 </div>
                             </div>
                         </div>
@@ -52,6 +77,8 @@
     },
     data() {
       return {
+        showingItems: [],
+
         stock: [],
         results: [],
         stockFuse: null,
@@ -98,6 +125,12 @@
       }
     },
     methods: {
+      showItems(items) {
+        this.showingItems = items;
+      },
+      removeItems() {
+        this.showingItems = [];
+      },
       //loadStock() {
       //  axios.get(this.apiUrl()+'/stocks').then(response => {
       //    this.stock = response.data;
@@ -193,5 +226,20 @@
         overflow: auto
         max-width: 500px
         max-height: 500px
+
+    .items-container
+        position: absolute
+        margin-left: 478px
+        margin-top: -9px
+        border-radius: 2px
+        padding: 7px
+        width: 330px
+
+    .button-thin
+        height: 24px
+        padding: 2px
+        padding-right: 10px
+        padding-left: 10px
+        font-family: BlinkMacSystemFont, -apple-system, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", "Helvetica", "Arial", sans-serif
 
 </style>
