20126059abb34e35a2d6a37589ab1e3fad98e6c3
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-11-07 14:37:31 +0300

    Fix HTTPs pagination

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS','on');
     }
 
     /**
