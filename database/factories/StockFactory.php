714a1ee5560c6e644ac3b39f696fa0850a8fd910
Shafiq al-Shaar <sean.spilot@gmail.com>
2019-11-11 11:17:20 +0300

    Add stock factory

--- database/factories/StockFactory.php
+++ database/factories/StockFactory.php
@@ -5,6 +5,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Stock::class, function (Faker $faker) {
     return [
-        'description' => $faker->text,
+        'description' => $faker->text(191),
+        'code' => null,
+        'rack_number' => rand(1, 999),
     ];
 });
