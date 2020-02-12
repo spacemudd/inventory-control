fdf82af0974991bb637d65b26032d28e12469cd5
Shafiq al-Shaar <sean.spilot@gmail.com>
2020-02-12 14:04:17 +0300

    Fix signature positions

--- resources/views/pdf/cost-approvals/signatures-block.blade.php
+++ resources/views/pdf/cost-approvals/signatures-block.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="row" style="margin-top:80px;">
         <div class="col-4-sm">
-        <p style="font-size:11px;padding-bottom:50px;">Prepared by:</p>
+            <p style="font-size:11px;padding-bottom:50px;">Prepared by:</p>
             <div class="center" style="border-top:2px solid black;width:100%;font-size:10px;">
                 <strong>
                     Ashraf Saeed<br/>
@@ -9,6 +9,18 @@
                 </strong>
             </div>
         </div>
+        @if ($data->approver_two_id)
+            <div class="col-4-sm"></div>
+            <div class="col-4-sm center">
+                <div style="border-top:2px solid black;width:100%;font-size:10px;margin-top:90px;">
+                    <strong>
+                        {{ $data->approver_two->name }}
+                        <br/>
+                        {{ $data->approver_two->designation }}
+                    </strong>
+                </div>
+            </div>
+        @endif
     </div>
     <div class="row" style="margin-top:80px;">
         @if ($data->approver_one_id || $data->approver_two_id || $data->approver_three_id)
@@ -25,19 +37,9 @@
                 </div>
             </div>
         @endif
-        @if ($data->approver_two_id)
-            <div class="col-4-sm center">
-                <div style="border-top:2px solid black;width:100%;font-size:10px;">
-                    <strong>
-                        {{ $data->approver_two->name }}
-                        <br/>
-                        {{ $data->approver_two->designation }}
-                    </strong>
-                </div>
-            </div>
-        @endif
 
         @if ($data->approver_three_id)
+            <div class="col-4-sm center"></div>
             <div class="col-4-sm center">
                 <div style="border-top:2px solid black;width:100%;font-size:10px;">
                     <strong>
