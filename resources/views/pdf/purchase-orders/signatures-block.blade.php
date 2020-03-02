411b032a2dee1a2847f3c2f6a955decbdaf981d9
Shafiq al-Shaar <sean.spilot@gmail.com>
2020-03-02 17:21:05 +0300

    Fixed

--- resources/views/pdf/purchase-orders/signatures-block.blade.php
+++ resources/views/pdf/purchase-orders/signatures-block.blade.php
@@ -1,24 +1,21 @@
 <div>
     {{-- <strong>For and on behalf of Arab National Bank</strong> --}}
-    <div class="row" style="margin-top:80px;letter-spacing: 0.5px;">
+    <div class="row" style="margin-top:80px;">
         @if ($data->approver_one)
-            <div class="col-4-sm center">
-                <div style="border-top:2px solid black;width:100%;font-size:20px;">
+            <div class="col-6-sm">
+                <div style="width:100%;font-size:20px;">
                     <strong>{{ optional($data->approver_one)->name }}</strong><br/>
-                    <strong><span style="font-size:12px;">{{ optional($data->approver_one)->designation }}</span></strong>
+                    {{ optional($data->approver_one)->designation }}
                 </div>
             </div>
         @endif
-        <div class="col-4-sm center">
+        <div class="col-2-sm center">
         </div>
         @if ($data->approver_two)
-        <div class="col-4-sm center">
-            <div style="border-top:2px solid black;width:100%;font-size:20px;">
-                <strong>
-                    {{ optional($data->approver_two)->name }}
-                    <br/>
-                    <span style="font-size:12px;">{{ optional($data->approver_two)->designation }}</span>
-                </strong>
+        <div class="col-4-sm">
+            <div style="width:100%;font-size:20px;">
+                <strong>{{ optional($data->approver_two)->name }}</strong><br/>
+                {{ optional($data->approver_two)->designation }}
             </div>
         </div>
         @endif
