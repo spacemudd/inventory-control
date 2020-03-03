e58013f4557e524fef25645a1beef188207940f4
Shafiq al-Shaar <sean.spilot@gmail.com>
2020-03-03 14:01:52 +0300

    Done

--- resources/views/pdf/purchase-orders/signatures-block.blade.php
+++ resources/views/pdf/purchase-orders/signatures-block.blade.php
@@ -2,14 +2,14 @@
     {{-- <strong>For and on behalf of Arab National Bank</strong> --}}
     <div class="row" style="margin-top:80px;">
         @if ($data->approver_one)
-            <div class="col-6-sm">
+            <div class="col-4-sm">
                 <div style="width:100%;font-size:20px;">
                     <strong style="font-family:Calibri;">{{ optional($data->approver_one)->name }}</strong><br/>
                     {{ optional($data->approver_one)->designation }}
                 </div>
             </div>
         @endif
-        <div class="col-2-sm center">
+        <div class="col-4-sm center">
         </div>
         @if ($data->approver_two)
         <div class="col-4-sm">
