875c3c6539ea322d773bbcae7549ecff0b7dfa27
Shafiq al-Shaar <shafiqalshaar@gmail.com>
2020-03-19 14:04:41 +0300

    CA LINE

--- app/CostApprovalLine.php
+++ app/CostApprovalLine.php
@@ -8,6 +8,10 @@ class CostApprovalLine extends Model
 {
     protected $guarded = ['id'];
 
+    protected $casts = [
+        'lump_sum' => 'boolean',
+    ];
+
     public function getTotalPriceAttribute()
     {
     	return number_format($this->unit_price * $this->qty, 2);
