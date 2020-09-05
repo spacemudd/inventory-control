fbeffc8f73f5b887f8481e51e21e295c1d641dc3
Shafiq <shafiqalshaar@gmail.com>
2020-09-05 12:38:07 +0300

    Ok

--- app/Clarimount/Service/InventoryPurchaseOrderService.php
+++ app/Clarimount/Service/InventoryPurchaseOrderService.php
@@ -26,8 +26,8 @@ class InventoryPurchaseOrderService
         
         $poRequest = $request->except('_token');
         $poRequest['status'] = PurchaseOrder::APPROVED;
-        $poRequest['approver_two_id'] = optional(Employee::where('name', 'LIKE', '%Saleh N. Al-Zunaidi%')->first())->id;
-        $poRequest['approver_one_id'] = optional(Employee::where('name', 'LIKE', 'Ashraf Saeed')->first())->id;
+        $poRequest['approver_one_id'] = optional(Employee::where('name', 'LIKE', '%Saleh N. Al-Zunaidi%')->first())->id;
+        $poRequest['approver_two_id'] = optional(Employee::where('name', 'LIKE', 'Ashraf Saeed')->first())->id;
         $poRequest['created_by_id'] = auth()->user()->id;
 
         return PurchaseOrder::create($poRequest);
