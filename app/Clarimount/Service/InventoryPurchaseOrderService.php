42395b5013912dfbf9f098f36db7095f463b1dcf
Shafiq <shafiqalshaar@gmail.com>
2020-09-05 14:21:25 +0300

    Fix PO saving

--- app/Clarimount/Service/InventoryPurchaseOrderService.php
+++ app/Clarimount/Service/InventoryPurchaseOrderService.php
@@ -25,7 +25,7 @@ class InventoryPurchaseOrderService
         ]);
 
         $poRequest = $request->except('_token');
-        $poRequest['status'] = PurchaseOrder::SAVED;
+        $poRequest['status'] = PurchaseOrder::NEW;
         $poRequest['approver_one_id'] = optional(Employee::where('name', 'LIKE', '%Saleh N. Al-Zunaidi%')->first())->id;
         $poRequest['approver_two_id'] = optional(Employee::where('name', 'LIKE', 'Ashraf Saeed')->first())->id;
         $poRequest['created_by_id'] = auth()->user()->id;
