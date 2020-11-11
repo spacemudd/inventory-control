e4f1d9364cfdd31b1cd2e745a3cf242095e37b50
Shafiq <shafiqalshaar@gmail.com>
2020-11-11 12:33:27 +0300

    Decrease distance in PDF

--- resources/views/pdf/cost-approvals/signatures-block.blade.php
+++ resources/views/pdf/cost-approvals/signatures-block.blade.php
@@ -1,5 +1,5 @@
 <div style="font-family:Calibri;">
-    <div class="row" style="margin-top:30px;">
+    <div class="row" style="margin-top:15px;">
         <div class="col-4-sm">
             <p style="font-size:11px;padding-bottom:50px;font-family: Calibri">Prepared by:</p>
             <div class="center" style="border-top:2px solid black;width:100%;font-size:10px;">
